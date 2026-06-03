a7234ec5e34092d6f01c003c7ab91d6adf8c0be2
Fabiano Vega <vegafabiano.fv@gmail.com>
2026-06-03 07:41:19 -0300

    criado tabela usuario

--- model.py
+++ model.py
@@ -1,7 +1,23 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Float, Boolean, Text
 from sqlalchemy.orm import declarative_base
 
 # conexão do banco
 db = create_engine("sqlite:///banco.db")
 
 Base = declarative_base()
+
+# tabelas a serem criadas
+#usuario
+#medicamentos
+#lote
+#movimentações
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    senha = Column(String(100), nullable=False)
+    admin = Column(Boolean, default=False)
+    funcao = Column(String(100), nullable=False)
+    ativo = Column(Boolean, default=True)
