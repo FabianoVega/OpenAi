67c54bf9e1e2ba8959c8aca1819fb2407134a6f8
Fabiano Vega <vegafabiano.fv@gmail.com>
2026-06-29 22:01:44 -0300

    Adicionado criação de usuario

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -1,7 +1,43 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.dependences import pegar_sessao
+from database.model import Usuario,db
+from app.dependences import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-@auth_router.post("/login")
-async def login():
-    return {"message": "login bem sucedido"}
+@auth_router.get("/")
+async def home():
+
+    """Rota home para autenticação"""
+    return {"message": "autenticação bem sucedida"}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str, funcao: str, session = Depends(pegar_sessao)):
+    """Rota para criar uma nova conta de usuário"""
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if usuario: #quer dizer que o email já existe
+        return {"message": "Email já cadastrado"}
+    else:
+        novo_usuario = Usuario(email=email, senha=senha, nome=nome, funcao=funcao)
+        session.add(novo_usuario)
+        session.commit()
+    return {"message": f"Conta criada com sucesso para o email: {email}"}
+
+
+
+
+# @auth_router.post("/criar_conta")
+# async def criar_conta(email: str, senha: str):
+#     """Rota para criar uma nova conta de usuário"""
+#     Session = sessionmaker(bind=db)
+#     session = Session()
+#     usuario = session.query(Usuario).filter(Usuario.email == email).first()
+#     if usuario: #quer dizer que o email já existe
+#         return {"message": "Email já cadastrado"}
+#     else:
+#         novo_usuario = Usuario(email=email, senha=senha)
+#         session.add(novo_usuario)
+#         session.commit()
+#         session.refresh(novo_usuario)
+#     return {"message": f"Conta criada com sucesso para o email: {email}"}
+
--- database/model.py
+++ database/model.py
@@ -30,42 +30,44 @@ class Usuario(Base):
         self.funcao = funcao
         self.ativo = ativo 
 
-    class Medicamento(Base):
-        __tablename__ = "medicamentos"
-        id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-        nome = Column(String(100), nullable=False)
-        descricao = Column(Text, nullable=True)
-        lote_id = Column(Integer, ForeignKey("lotes.id"), nullable=True)
+class Medicamento(Base):
+    __tablename__ = "medicamentos"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    nome = Column(String(100), nullable=False)
+    descricao = Column(Text, nullable=True)
+    lote_id = Column(Integer, ForeignKey("lotes.id"), nullable=True)
 
-        def __init__(self, nome, descricao="", lote_id=0):
-            self.nome = nome
-            self.descricao = descricao
-            self.lote_id = lote_id
+    def __init__(self, nome, descricao="", lote_id=0):
+        self.nome = nome
+        self.descricao = descricao
+        self.lote_id = lote_id
 
-    class Lote(Base):
-        __tablename__ = "lotes"
-        id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-        numero_lote = Column(String(100), nullable=False)
-        data_validade = Column(DateTime, nullable=False)
-        quantidade = Column(Integer, nullable=False)
+class Lote(Base):
+    __tablename__ = "lotes"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    numero_lote = Column(String(100), nullable=False)
+    data_validade = Column(DateTime, nullable=False)
+    quantidade = Column(Integer, nullable=False)
+    medicamento_id = Column(Integer, ForeignKey("medicamentos.id"), nullable=False)
 
-        def __init__(self, numero_lote, data_validade, quantidade):
-            self.numero_lote = numero_lote
-            self.data_validade = data_validade
-            self.quantidade = quantidade
+    def __init__(self, numero_lote, data_validade, quantidade, medicamento_id):
+        self.numero_lote = numero_lote
+        self.data_validade = data_validade
+        self.quantidade = quantidade
+        self.medicamento_id = medicamento_id
 
-    class Movimentacao(Base):
-        __tablename__ = "movimentacoes"
-        id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-        medicamento_id = Column(Integer, ForeignKey("medicamentos.id"), nullable=False)
-        usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
-        tipo_movimentacao = Column(String(50), nullable=False)  # Entrada ou Saída
-        quantidade = Column(Integer, nullable=False)
-        data_movimentacao = Column(DateTime, nullable=False)
+class Movimentacao(Base):
+    __tablename__ = "movimentacoes"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    medicamento_id = Column(Integer, ForeignKey("medicamentos.id"), nullable=False)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+    tipo_movimentacao = Column(String(50), nullable=False)  # Entrada ou Saída
+    quantidade = Column(Integer, nullable=False)
+    data_movimentacao = Column(DateTime, nullable=False)
 
-        def __init__(self, medicamento_id, usuario_id, tipo_movimentacao, quantidade, data_movimentacao):
-            self.medicamento_id = medicamento_id
-            self.usuario_id = usuario_id
-            self.tipo_movimentacao = tipo_movimentacao
-            self.quantidade = quantidade
-            self.data_movimentacao = data_movimentacao
+    def __init__(self, medicamento_id, usuario_id, tipo_movimentacao, quantidade, data_movimentacao):
+        self.medicamento_id = medicamento_id
+        self.usuario_id = usuario_id
+        self.tipo_movimentacao = tipo_movimentacao
+        self.quantidade = quantidade
+        self.data_movimentacao = data_movimentacao
